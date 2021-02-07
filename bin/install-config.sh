2f84d73dd00ce9c7ec66882a2b072ec20d4d6f89
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-02-07 23:44:46 +0000

    install: don't remove repo if exists

--- bin/install-config.sh
+++ bin/install-config.sh
@@ -4,8 +4,6 @@ set -ex
 shopt -s expand_aliases
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-# Remove repo if exists
-[ -d "$HOME/.dotfiles" ] && rm -rf "$HOME/.dotfiles"
 git clone --bare https://github.com/KapJI/dotfiles.git ~/.dotfiles
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
