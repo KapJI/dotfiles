d342a0478bd0fb3358722f276b4103dd39400897
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-02-07 13:48:20 +0000

    install: expand_aliases

--- bin/install-config.sh
+++ bin/install-config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+shopt -s expand_aliases
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare https://github.com/KapJI/dotfiles.git ~/.dotfiles
