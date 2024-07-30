6b1ed7d557a7270cf1e275ad199584d2b063d52b
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-07-30 16:54:29 +0100

    myconfig: update setup for centos

--- .mysrc/myconfig_lib/centos.sh
+++ .mysrc/myconfig_lib/centos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function install_centos_packages() {
+    setup_proxy
     local dnf_packages=(
         fd-find
         fzf
@@ -16,8 +17,16 @@ function install_centos_packages() {
     if ! command_exists et; then
         sudo feature install et
     fi
+    cargo install lsd
+}
+
+function setup_proxy() {
+    export CURL_CONFIG=$(fwdproxy-config curl)
+    export http_proxy="http://fwdproxy:8080"
+    export https_proxy="http://fwdproxy:8080"
 }
 
 function setup_centos() {
     echo "setup centos"
+    sudo ln -sfn /usr/bin/pinentry-tty /usr/local/bin/pinentry-current
 }
