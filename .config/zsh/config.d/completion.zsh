a0a2a10bf0069bf019a77e72ad2b546567d9724c
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-09-27 18:34:06 +0100

    zshrc: add fzf-tab plugin

--- .config/zsh/config.d/completion.zsh
+++ .config/zsh/config.d/completion.zsh
@@ -4,7 +4,31 @@
 zstyle ':completion:*' matcher-list 'm:{[:lower:]-_}={[:upper:]_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # Disable hashing for list of executables
-zstyle ":completion:*:commands" rehash 1
+zstyle ':completion:*:commands' rehash 1
+
+# Disable sorting when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+
+# Set descriptions format without escape sequences (like '%F{red}%d%f')
+zstyle ':completion:*' format '[%d]'
+
+# Remove -- separator between columns
+zstyle ':completion:*' list-separator ''
+
+# Set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# Force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+
+# Preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:__enhancd::cd:*' fzf-preview 'eza -1 --color=always --icons=always $realpath'
+
+# Switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+
+# Remove prefix from every option
+zstyle ':fzf-tab:*' prefix ''
 
 # Fixed slow git autocompletion.
 __git_files () {
