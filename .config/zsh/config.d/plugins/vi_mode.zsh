0d9fa75c1039bc479387305d0beff5ff522eecf0
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-09-27 15:33:48 +0100

    zsh: install fancy-ctrl-z plugin

--- .config/zsh/config.d/plugins/vi_mode.zsh
+++ .config/zsh/config.d/plugins/vi_mode.zsh
@@ -2,4 +2,5 @@
 function zvm_after_init() {
   eval "$(fzf --zsh)"
   source "$(antidote path ohmyzsh/ohmyzsh)/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh"
+  zvm_bindkey viins "^Z" fancy-ctrl-z
 }
