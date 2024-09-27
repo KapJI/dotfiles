b817448442e58651267c293505a07f255d3659a1
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-09-27 18:34:06 +0100

    zshrc: add fzf-tab plugin

--- .config/zsh/config.d/plugins/vi_mode.zsh
+++ .config/zsh/config.d/plugins/vi_mode.zsh
@@ -1,6 +1,5 @@
 # Initialise fzf here for compatibility with vi mode
 function zvm_after_init() {
   eval "$(fzf --zsh)"
-  source "$(antidote path ohmyzsh/ohmyzsh)/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh"
   zvm_bindkey viins "^Z" fancy-ctrl-z
 }
