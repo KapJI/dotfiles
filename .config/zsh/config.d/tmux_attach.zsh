e620de86339d70652c1d88184a95fb83c7c66147
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-09-29 20:37:16 +0100

    zshrc: simplify tmux attach

--- .config/zsh/config.d/tmux_attach.zsh
+++ .config/zsh/config.d/tmux_attach.zsh
@@ -6,9 +6,5 @@ else
 fi
 
 if [[ ${VSCODE_TASK:-} == false ]] && [ -t 0 ] && [ -z "$TMUX" ] && command -v tmux >/dev/null 2>&1; then
-    if tmux has-session -t auto >/dev/null 2>&1; then
-        exec tmux -2 attach -t auto
-    else
-        exec tmux -2 new-session -s auto
-    fi
+    tmux new -A -s auto
 fi
