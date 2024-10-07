efb355ea1251e51e3392440fc744b36213284cab
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-07 23:41:09 +0100

    chezmoi: add script to set pwsh execution policy

---
+++ home/.chezmoiscripts/windows/run_once_03-check-execution-policy.ps1
@@ -0,0 +1,3 @@
+Write-Host "Set execution policy to RemoteSigned." 
+Write-Host "Open an admin PowerShell and run the following command:"
+Write-Host "`nSet-ExecutionPolicy RemoteSigned -Scope CurrentUser`n"
