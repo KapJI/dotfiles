8e8f0eb3179e4cad0eb0f66a845c7b3493fe707e
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 18:34:26 +0100

    chezmoi/windows: better decrypt script

--- home/.chezmoiscripts/windows/run_onchange_before_10-decrypt-private-key.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_10-decrypt-private-key.ps1
@@ -1,14 +1,22 @@
-$OUTPUT = "$HOME\.config\chezmoi\key.txt"
+$CONFIG_DIR = "$HOME\.config\chezmoi"
+$OUTPUT = "$CONFIG_DIR\key.txt"
 
 if (!(Test-Path $OUTPUT)) {
     # Reload Path
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
     # Create the directory if it doesn't exist
-    New-Item -ItemType Directory -Path "$HOME\.config\chezmoi" -Force
+    if (!(Test-Path -PathType container $CONFIG_DIR)) {
+        New-Item -ItemType Directory -Path $CONFIG_DIR
+    }
 
     # Decrypt the key.txt.age file
-    chezmoi age decrypt --output $OUTPUT --passphrase "$(chezmoi source-path)/key.txt.age"
+    for ($i = 0; $i -le 5; $i++) {
+        chezmoi age decrypt --output $OUTPUT --passphrase "$(chezmoi source-path)/key.txt.age"
+        if ($?) {
+            break
+        }
+    }
 
     # Set permissions so only the current user has read and write access
     $user = "$env:USERDOMAIN\$env:USERNAME"
