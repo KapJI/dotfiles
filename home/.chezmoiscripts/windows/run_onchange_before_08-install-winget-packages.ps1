8c9fea22ec1aa0075f0e92521c9216493db8f63a
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 15:10:00 +0100

    chezmoi/windows: fix scripts

--- home/.chezmoiscripts/windows/run_onchange_before_08-install-winget-packages.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_08-install-winget-packages.ps1
@@ -1,5 +1,9 @@
 # Install ssh agent config
-mkdir $env:USERPROFILE\AppData\Local\1Password\config\ssh\
-cp $(& $env:USERPROFILE\.local\bin\chezmoi source-path)\.chezmoitemplates\1password_agent.toml $env:USERPROFILE\AppData\Local\1Password\config\ssh\agent.toml
+$path = "$env:USERPROFILE\AppData\Local\1Password\config\ssh"
+if (!(Test-Path -PathType container $path)) {
+      New-Item -ItemType Directory -Path $path
+}
+$sourcePath = & $env:USERPROFILE\.local\bin\chezmoi source-path
+cp "$sourcePath/.chezmoitemplates/1password_agent.toml" $path\agent.toml
 winget install -e --id AgileBits.1Password
 winget install -e --id Microsoft.PowerShell
