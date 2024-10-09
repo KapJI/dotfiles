c699b9980ccb790eee633cb5fd0d68df5e22caeb
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 16:01:41 +0100

    chezmoi/windows: don't enable WSL

--- home/.chezmoiscripts/windows/run_onchange_before_03-enable-dev-mode.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_03-enable-dev-mode.ps1
@@ -26,11 +26,11 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
         # Add registry value to enable Developer Mode
         New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
     }
-    $feature = Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online
-    if ($feature -and ($feature.State -eq "Disabled"))
-    {
-        Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -LimitAccess -NoRestart
-    }
+    # $feature = Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online
+    # if ($feature -and ($feature.State -eq "Disabled"))
+    # {
+    #     Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -LimitAccess -NoRestart
+    # }
     Write-Host "Developer Mode enabled successfully. Symlinks are now enabled for non-admin users."
 
     if ($WaitForKey)
