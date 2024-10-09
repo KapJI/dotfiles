e4928485113e657a66a2a71d0120b52dbb997462
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 14:56:37 +0100

    chezmoi/windows: install powershell core from winget

--- home/.chezmoiscripts/windows/run_onchange_before_08-install-winget-packages.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_08-install-winget-packages.ps1
@@ -2,3 +2,4 @@
 mkdir $env:USERPROFILE\AppData\Local\1Password\config\ssh\
 cp $(& $env:USERPROFILE\.local\bin\chezmoi source-path)\.chezmoitemplates\1password_agent.toml $env:USERPROFILE\AppData\Local\1Password\config\ssh\agent.toml
 winget install -e --id AgileBits.1Password
+winget install -e --id Microsoft.PowerShell
