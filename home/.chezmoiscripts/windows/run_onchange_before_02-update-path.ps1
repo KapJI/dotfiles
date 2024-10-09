69b628dfd4d1cfa0e9c02678265f4f9bbc31a27e
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 11:05:37 -0700

    chezmoi/windows: add Sublime to Path

--- home/.chezmoiscripts/windows/run_onchange_before_02-update-path.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_02-update-path.ps1
@@ -3,6 +3,13 @@ $currentProfile = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVari
 if ($currentProfile -notlike "*$env:USERPROFILE\.local\bin*") {
     [Environment]::SetEnvironmentVariable(
         "Path",
-        $currentProfile + ";$env:USERPROFILE\.local\bin",
+        $currentProfile + ";$env:USERPROFILE\.local\bin;$env:ProgramFiles\Sublime Text",
+        [EnvironmentVariableTarget]::User)
+}
+
+if ($currentProfile -notlike "*$env:ProgramFiles\Sublime Text*") {
+    [Environment]::SetEnvironmentVariable(
+        "Path",
+        $currentProfile + ";$env:ProgramFiles\Sublime Text",
         [EnvironmentVariableTarget]::User)
 }
