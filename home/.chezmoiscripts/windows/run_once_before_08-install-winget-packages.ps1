3f2300217447d59674698cc98443d1443ce34804
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-09 14:39:38 +0100

    chezmoi/windows: fix typo in scoop packages script

--- home/.chezmoiscripts/windows/run_once_before_08-install-winget-packages.ps1
+++ home/.chezmoiscripts/windows/run_once_before_08-install-winget-packages.ps1
@@ -1,4 +1,4 @@
 # Install ssh agent config
 mkdir $env:USERPROFILE\AppData\Local\1Password\config\ssh\
-cp $($env:USERPROFILE\.local\bin\chezmoi source-path)\.chezmoitemplates\1password_agent.toml $env:USERPROFILE\AppData\Local\1Password\config\ssh\agent.toml
+cp $(& $env:USERPROFILE\.local\bin\chezmoi source-path)\.chezmoitemplates\1password_agent.toml $env:USERPROFILE\AppData\Local\1Password\config\ssh\agent.toml
 winget install -e --id AgileBits.1Password
