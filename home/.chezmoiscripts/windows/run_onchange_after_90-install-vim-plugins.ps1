63a11901ad8b5144373e5941c1430413feda650c
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-10 11:56:52 +0100

    chezmoi/windows: install vim-plug by chezmoiexternal

--- home/.chezmoiscripts/windows/run_onchange_after_90-install-vim-plugins.ps1
+++ home/.chezmoiscripts/windows/run_onchange_after_90-install-vim-plugins.ps1
@@ -1,9 +1,5 @@
 # Reload Path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
-# Install vim plug
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
 # Update nvim plugins
 nvim +PlugUpgrade +PlugUpdate +qall
