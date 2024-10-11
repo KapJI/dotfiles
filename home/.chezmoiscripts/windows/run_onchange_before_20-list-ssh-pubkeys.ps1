524765b164023fee059bb6fd6f04f28c702c264d
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-11 12:13:47 +0100

    chezmoi/windows: copy 1password config in list pubkeys script

--- home/.chezmoiscripts/windows/run_onchange_before_20-list-ssh-pubkeys.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_20-list-ssh-pubkeys.ps1
@@ -1,6 +1,14 @@
 # Reload Path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
+# Install ssh agent config
+$configDir = "$env:USERPROFILE\AppData\Local\1Password\config\ssh"
+if (!(Test-Path -PathType container $configDir)) {
+      New-Item -ItemType Directory -Path $configDir
+}
+$sourcePath = chezmoi source-path
+cp "$sourcePath/.chezmoitemplates/1password_agent.toml" $configDir\agent.toml
+
 $OUTPUT = "$HOME\.config\chezmoi\ssh_pubkeys.txt"
 
 for ($i = 0; $i -le 5; $i++) {
