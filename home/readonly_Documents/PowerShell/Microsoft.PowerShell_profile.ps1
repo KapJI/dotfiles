21a850f830e5a89c77d6f8bc40a44e9c862ef493
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-13 19:05:17 +0100

    pwsh profile: lazy load modules

--- home/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,55 +2,50 @@
 
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 oh-my-posh init pwsh --config ~/Documents/ohmyposh-themes/thecyberden.omp.json | Invoke-Expression
-$env:POSH_GIT_ENABLED = $true
+
+# Environment
 $env:BAT_PAGER = "ov"
 $env:BAT_THEME = "1337"
-# Enable Zsh like completion
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
-Set-PSReadLineKeyHandler -Key Ctrl+k -Function ForwardDeleteLine
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-# Aliases
-Remove-Item -Force Alias:sl
-New-Alias sl subl
-New-Alias vim nvim
-New-Alias czm chezmoi
-Add-Alias czmcd "cd $(chezmoi source-path)"
-# Git aliases from oh-my-zsh
-New-Alias g git
-# Add-Alias requires running `Install-Module -Name posh-alias`
-Add-Alias ga "git add"
-Add-Alias gb "git branch"
-Remove-Item -Force Alias:gc
-Add-Alias gc "git commit"
-Add-Alias gco "git checkout"
-Add-Alias gcl "git clone"
-Add-Alias gcam "git commit --all --message"
-Remove-Item -Force Alias:gcm
-Add-Alias gcm "git commit --message"
-Add-Alias gcn! "git commit --verbose --no-edit --amend"
-Add-Alias gcan! "git commit --verbose --all --no-edit --amend"
-Add-Alias gd "git diff"
-Add-Alias gf "git fetch"
-Add-Alias gpl "git pull"
-Add-Alias gplra "git pull --rebase --autostash"
-Remove-Item -Force Alias:gp
-Add-Alias gp "git push"
-Add-Alias gpf! "git push --force"
-Add-Alias gr "git remote"
-Add-Alias grb "git rebase"
-Add-Alias grs "git restore"
-Add-Alias grm "git rm"
-Add-Alias gsh "git show"
-Add-Alias gst "git status"
-Add-Alias gsl "git sl"
-# fzf
-$env:FZF_DEFAULT_OPTS="--ansi --height=40% --layout=reverse"
-Import-Module posh-git
-Import-Module Terminal-Icons
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Delayed modules load
+$DelayedLoadProfileRunspace = [RunspaceFactory]::CreateRunspace()
+$DelayedLoadProfile = [PowerShell]::Create()
+$DelayedLoadProfile.Runspace = $DelayedLoadProfileRunspace
+$DelayedLoadProfileRunspace.Open()
+[void]$DelayedLoadProfile.AddScript({
+    # Preload modules
+    Import-Module posh-alias
+    Import-Module PSFzf
+    Import-Module PSReadLine
+})
+[void]$DelayedLoadProfile.BeginInvoke()
+$null = Register-ObjectEvent -InputObject $DelayedLoadProfile -EventName InvocationStateChanged -Action {
+    # Enable Zsh like completion
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+    Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
+    Set-PSReadLineKeyHandler -Key Ctrl+k -Function ForwardDeleteLine
+
+    . $HOME\Documents\Powershell\aliases.ps1
+
+    # Init modules
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    $env:FZF_DEFAULT_OPTS="--ansi --height=40% --layout=reverse"
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+    # Load completion
+    . $HOME\Documents\Powershell\Completion\chezmoi.ps1
+    Register-ArgumentCompleter -CommandName 'czm' -ScriptBlock ${__chezmoiCompleterBlock}
+    . $HOME\Documents\Powershell\Completion\delta.ps1
+    . $HOME\Documents\Powershell\Completion\ov.ps1
+    . $HOME\Documents\Powershell\Completion\ruff.ps1
+
+    # Cleanup
+    $DelayedLoadProfile.Dispose()
+    $DelayedLoadProfileRunspace.Close()
+    $DelayedLoadProfileRunspace.Dispose()
+}
 
 function head {
     param(
@@ -79,17 +74,9 @@ function tail {
     }
 }
 
-function df {
-    get-volume
-}
-
 function which ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-function fd {
-    fd.exe --hidden $args
 }
 
 function bathelp {
@@ -123,10 +110,3 @@ function touch {
         }
     }
 }
-
-# Load completion
-. $HOME\Documents\Powershell\Completion\chezmoi.ps1
-Register-ArgumentCompleter -CommandName 'czm' -ScriptBlock ${__chezmoiCompleterBlock}
-. $HOME\Documents\Powershell\Completion\delta.ps1
-. $HOME\Documents\Powershell\Completion\ov.ps1
-. $HOME\Documents\Powershell\Completion\ruff.ps1
