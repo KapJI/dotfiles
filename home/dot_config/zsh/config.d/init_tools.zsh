fe397b6831d99a8c459ba6f667b162204d486bc5
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2026-04-26 20:51:02 +0100

    zsh: drop broot launcher source

--- home/dot_config/zsh/config.d/init_tools.zsh
+++ home/dot_config/zsh/config.d/init_tools.zsh
@@ -4,6 +4,3 @@ source "$ZDOTDIR/config.d/plugins/p10k.zsh"
 
 # Enable Iterm2 shell integration
 source "$ZDOTDIR/config.d/plugins/iterm2_shell_integration.zsh"
-
-# Enable broot
-source ${HOME}/.config/broot/launcher/bash/br
