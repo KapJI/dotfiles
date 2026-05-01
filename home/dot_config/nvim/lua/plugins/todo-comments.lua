64a9e8937323adbcdf09f1ce93f98b14a496774a
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2026-05-01 23:28:11 +0100

    nvim: add todo-comments for TODO/FIX/HACK/NOTE highlighting
    Highlights keyword comments with colored bg + gutter sign per category.
    <leader>ct opens an fzf-lua picker of every TODO in the project (reuses
    existing fzf-lua); ]t / [t jump between TODOs in the current buffer.
    Defaults handle the standard keyword set (TODO, FIX, HACK, WARN, PERF,
    NOTE, TEST) and Nerd-Font icons.

---
+++ home/dot_config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,21 @@
+-- todo-comments.nvim: highlight TODO / FIX / HACK / NOTE / PERF / WARN / TEST
+-- comments with colored signs + provide commands to list them across the project.
+--
+-- Defaults are sensible (keyword set, colors, icons via Nerd Font), so just
+-- wire keys and let the plugin do its thing.
+return {
+  {
+    "folke/todo-comments.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TodoFzfLua", "TodoQuickFix", "TodoLocList" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ct", "<cmd>TodoFzfLua<cr>",                            desc = "List TODOs (project)" },
+      { "]t",         function() require("todo-comments").jump_next() end, desc = "Next TODO comment" },
+      { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous TODO comment" },
+    },
+    opts = {
+      signs = true, -- gutter sign for each TODO line
+    },
+  },
+}
