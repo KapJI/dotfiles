24d1928a06c2ea8edebe59209d61ce1c37ddc98d
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2026-05-10 00:04:37 +0100

    zsh: fzf Alt+C uses fd for fast recursive dir picker
    Sets FZF_ALT_C_COMMAND to the same fd invocation as the file picker
    (--type d). Default Alt+C uses find, which is slow on deep trees and
    ignores gitignore; this gives parity with the Ctrl+T file picker's
    exclusions and makes Alt+C usable from $HOME.

--- home/dot_config/zsh/config.d/plugins/fzf.zsh
+++ home/dot_config/zsh/config.d/plugins/fzf.zsh
@@ -4,6 +4,10 @@ eval "$(fzf --zsh)"
 # Configure fzf to use fd
 export FZF_DEFAULT_COMMAND="fd --type file --color=always --hidden --exclude .git --exclude .hg --exclude node_modules"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# Alt+C → recursive directory picker. Default uses `find`, which is slow
+# in deep trees and ignores gitignore; switch to fd for parity with the
+# file picker's exclusions.
+export FZF_ALT_C_COMMAND="fd --type d --color=always --hidden --exclude .git --exclude .hg --exclude node_modules"
 export FZF_DEFAULT_OPTS="--ansi --height=50% --layout=reverse"
 # export FZF_DEFAULT_OPTS="--ansi --height=40% --layout=reverse --preview-window 'right:60%' --preview 'if [ -d {} ]; then eza --tree --level=1 --color=always --icons=always {}; elif [ -f {} ]; then head -n 100 {} | chroma --style=emacs --filename={}; else echo {}; fi'"
 
